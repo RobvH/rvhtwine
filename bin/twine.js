ef4eaa4d5ab711da5b6b23c71c0cb084524147b7
Rob van Hoose <Rob.vanHoose@bankrate.com>
2018-06-07 00:43:09 -0400

    Add inquirer and credential-manager

--- bin/twine.js
+++ bin/twine.js
@@ -1,3 +1,13 @@
 #!/usr/bin/env node
 
-console.log("Hello Worlds!")
+const CredentialManager = require('../lib/credential-manager')
+
+async function main () {
+  const creds = new CredentialManager('rvhtwine')
+  
+  let [key, secret] = await creds.getKeyAndSecret()
+  
+  console.log(key, secret)
+}
+
+main()
